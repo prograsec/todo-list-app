0eee7bc970ba3f6e54de8403cf8e770af93fbc0c
Amina Asif <asifyaqoobamina@gmail.com>
2025-07-30 12:48:38 +0500

    Updated CSS Styles for UI Improvement.

--- TO DO list app/config/db.py
+++ TO DO list app/config/db.py
@@ -1,5 +1,7 @@
 
 from pymongo import MongoClient
 # MONGO_URI=("mongodb+srv://AminaAsif:Mongodb123@todocluster.pdfdjz3.mongodb.todo")
+#MONGO_URI=("mongodb+srv://AminaAsif:Mongodb123@todocluster.pdfdjz3.mongodb.net")
 MONGO_URI=("mongodb+srv://AminaAsif:Mongodb123@todocluster.pdfdjz3.mongodb.net")
+
 conn = MongoClient(MONGO_URI)
